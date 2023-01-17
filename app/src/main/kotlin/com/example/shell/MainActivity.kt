71384f6e9d1d0afbc1c5817c1317754f49c38d1c
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2023-01-17 15:59:21 +0330

    Update MainActivity.kt
    Added command RUN button and Textview

--- app/src/main/kotlin/com/example/shell/MainActivity.kt
+++ app/src/main/kotlin/com/example/shell/MainActivity.kt
@@ -5,30 +5,42 @@ import androidx.lifecycle.lifecycleScope
 import android.os.Bundle
 import android.util.Log
 import android.content.Context
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import android.text.method.ScrollingMovementMethod
 import com.example.shell.databinding.ActivityMainBinding
 import kotlinx.coroutines.async
 
 class MainActivity : AppCompatActivity() {
+    public val context = this@MainActivity
     init {
         lifecycleScope.launchWhenCreated {
-            val context = this@MainActivity
             copyAssets(context)
             arrayOf(
-            "getprop ro.vendor.product.model",
-            "getprop ro.build.version.release",
             "date",
-            "ls -R -1 -h -g ${context.filesDir.absolutePath}",
-            "cat /proc/cpuinfo", "cat /proc/meminfo",
-            "free", "cat /proc/version",
             "echo ${context.filesDir.absolutePath}"
             ).forEach { command ->
-                Toast.makeText(context, async { shell(command) }.await(), Toast.LENGTH_LONG).show()
+                val result = async { shell(command) }
+                Toast.makeText(context, result.await(), Toast.LENGTH_LONG).show()
             }
         }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var commandRun: Button = findViewById(R.id.button)
+    }
+    public fun commandRunner(v: View){
+        var Output: TextView = findViewById(R.id.textView2)
+        var command_container: EditText = findViewById(R.id.Command_container)
+        Output.setMovementMethod(ScrollingMovementMethod())
+        var command = command_container.getText().toString()
+        lifecycleScope.launchWhenCreated {
+            val result = async { shell(command) }
+            Output.setText(result.await())
+        }
     }
 }
